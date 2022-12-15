4224b5fd638c9768d98a39375cddb39603661929
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-12-15 00:42:51 -0800

    cleaned up the look of the code and added projects to the project board

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Main';
 import BuildWrkBnch from './pages/Workbench_build';
@@ -9,9 +9,31 @@ import Download from './pages/Download_build';
 import Header from './components/Header';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import Auth from './utils/auth'
+import { findAllProjects } from './utils/api';
 
 
 function App() {
+	const [projects, setProjects] = useState([]);
+  useEffect(() => {
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
+		if (!token) {
+			return false;
+		}
+		const getUserBuilds = async () => {
+			try {
+				const response = await findAllProjects(token);
+				if (!response.ok) {
+					throw new Error('something went wrong!');
+				}
+				const user = await response.json();
+				setProjects(user.builds);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		getUserBuilds();
+	}, []);
   return (
     <DndProvider backend={HTML5Backend}>
       <Router>
@@ -23,8 +45,8 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/wrk' element={<BuildWrkBnch />} />
-            <Route path='/Projects' element={<Projects />} />
-            <Route path='/export' element={<Download />} />
+            <Route path='/Projects' element={<Projects projects= {projects} />} />
+            <Route path='/export' element={<Download projects= {projects}/>} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Routes>
         </>
